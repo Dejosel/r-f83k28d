5b8382569adc8e20bcc84a702761dc874a80ca28
Dejosel <newjos7@gmail.com>
2018-06-14 19:22:22 -0500

    Add Function Contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor (){
+		super()
+		this.state = {
+			contador:0
+		}
+	}
+	aumentar (){
+		this.setState({contador:this.state.contador+1});
+	}
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+        <button id="inc" onClick={this.aumentar.bind(this)} value="Incrementar">Incrementa</button>
       </div>
     );
   }
